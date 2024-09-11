14711f72a050694e65e5ddae366e23228b74215c
Amirabbas Fazelinia <utilyre@gmail.com>
2024-09-11 11:57:34 +0330

    refactor(badge): use custom css class instead of extending tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,11 +2,7 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {
-			dropShadow: {
-				white: '0 2px 8px rgb(255 255 255 / 0.25)',
-			},
-		},
+		extend: {},
 	},
 	plugins: [],
 }
