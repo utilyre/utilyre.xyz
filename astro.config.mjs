911db6460b4da7daa2b92cf80476205dd5b7f17d
Amirabbas Fazelinia <utilyre@gmail.com>
2024-09-11 21:34:16 +0330

    build(pnpm): install prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://utilyre.github.io',
-  integrations: [tailwind(), icon()]
+  site: "https://utilyre.github.io",
+  integrations: [tailwind(), icon()],
 });
